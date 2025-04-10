620f5ce02234397325989a6e0b0ce89ba32ec93d
Yuriy Filipkov <yuriyfilipkov@gmail.com>
2025-04-11 00:23:44 +0200

    2nd commit

--- probe.py
+++ probe.py
@@ -67,3 +67,78 @@ print(f"Загальна сума заробітної плати: {total}, Се
 
 
 
+# from pathlib import Path
+
+# def total_salary(path):
+#     total = 0
+#     count = 0
+#     try:
+#         with open(path, 'r', encoding='utf-8') as file:
+#             for line in file:
+#                 line = line.strip()
+#                 if not line:
+#                     continue  # Пропустити порожні рядки
+#                 try:
+#                     parts = line.split(',', 1)  # Розділити тільки по першій комі
+#                     if len(parts) != 2:
+#                         raise ValueError
+#                     name, salary = parts
+#                     total += float(salary)
+#                     count += 1
+#                 except ValueError:
+#                     print(f"⚠️ Помилка при обробці рядка: {line}")
+#                     continue
+#         if count == 0:
+#             return (0, 0)
+#         average = round(total / count, 2)
+#         return (total, average)
+#     except FileNotFoundError:
+#         print("❌ Файл не знайдено.")
+#         return (0, 0)
+#     except Exception as e:
+#         print(f"❌ Сталася помилка: {e}")
+#         return (0, 0)
+
+# # Приклад використання функції
+# file_path = 'zp.txt'
+# total, average = total_salary(file_path)
+# print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
+
+from pathlib import Path
+
+def get_cats_info(path):
+    cats_list = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue  # пропускаємо порожні рядки
+                try:
+                    cat_id, name, age = line.split(',')
+                    cats_list.append({
+                        "id": cat_id,
+                        "name": name,
+                        "age": age
+                    })
+                except ValueError:
+                    print(f"⚠️ Некоректний рядок: {line}")
+                    continue
+        return cats_list
+    except FileNotFoundError:
+        print("❌ Файл не знайдено.")
+        return []
+    except Exception as e:
+        print(f"❌ Сталася помилка: {e}")
+        return []
+
+# Приклад використання:
+file_path = 'cats.txt'  # можна і абсолютний шлях до файлу, але він не буде працювати на GitHub
+cats_info = get_cats_info(file_path)
+print(cats_info)
+
+
+
+
